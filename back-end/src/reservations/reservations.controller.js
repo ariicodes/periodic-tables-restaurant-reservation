cca18a48dbecc9353f7b5b1cd95efd0564128c00
Ariela Israel <arielaisrael12@gmail.com>
2023-12-20 17:34:03 -0500

    middleware added

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -6,7 +6,7 @@ const asyncErrorBoundary = require('../errors/asyncErrorBoundary');
  */
 const reservationsExists = async (req, res, next) => {
 	const { date } = req.query;
-	let reservations = await service.list(date);
+	let reservations = await reservationsService.list(date);
 	if (reservations) {
 		res.locals.reservations = reservations;
 		return next();
@@ -15,6 +15,161 @@ const reservationsExists = async (req, res, next) => {
 		status: 404,
 		message: `Reservations can not be found.`,
 	});
+};
+
+/**
+ * Middleware to check for required fields
+ */
+const checkRequiredFields = (req, res, next) => {
+	const {
+		first_name,
+		last_name,
+		mobile_number,
+		reservation_date,
+		reservation_time,
+		people,
+	} = req.body.data || {};
+
+	const requiredFields = {
+		first_name: 'A first_name property is required.',
+		last_name: 'A last_name property is required.',
+		mobile_number: 'A mobile_number property is required.',
+		reservation_date: 'A reservation_date property is required.',
+		reservation_time: 'A reservation_time property is required.',
+		people: 'A people property is required.',
+	};
+
+	for (const field in requiredFields) {
+		if (!req.body.data.hasOwnProperty(field) || req.body.data[field] === '') {
+			return next({
+				status: 400,
+				message: requiredFields[field],
+			});
+		}
+	}
+	return next();
+};
+
+/**
+ * Middleware to check for valid types
+ */
+const checkTypes = (req, res, next) => {
+	const {
+		first_name,
+		last_name,
+		mobile_number,
+		reservation_date,
+		reservation_time,
+		people,
+	} = req.body.data || {};
+
+	const typeChecks = {
+		first_name: 'string',
+		last_name: 'string',
+		mobile_number: 'string',
+		reservation_date: 'string',
+		reservation_time: 'string',
+		people: 'number',
+	};
+
+	for (const field in typeChecks) {
+		if (typeof req.body.data[field] !== typeChecks[field]) {
+			return next({
+				status: 400,
+				message: `A ${field} property must be a ${typeChecks[field]}.`,
+			});
+		}
+	}
+	return next();
+};
+
+/**
+ * Middleware to check for valid date
+ */
+const checkDateValidity = (req, res, next) => {
+	const { reservation_date } = req.body.data || {};
+
+	if (reservation_date.length !== 10) {
+		return next({
+			status: 400,
+			message: `A reservation_date property must be a valid date.`,
+		});
+	}
+	if (reservation_date.includes('-') === false) {
+		return next({
+			status: 400,
+			message: `A reservation_date property must be a valid date.`,
+		});
+	}
+	if (reservation_date.includes('T') === true) {
+		return next({
+			status: 400,
+			message: `A reservation_date property must be a valid date.`,
+		});
+	}
+	if (reservation_date.includes(':') === true) {
+		return next({
+			status: 400,
+			message: `A reservation_date property must be a valid date.`,
+		});
+	}
+	if (reservation_date.includes('Z') === true) {
+		return next({
+			status: 400,
+			message: `A reservation_date property must be a valid date.`,
+		});
+	}
+	if (reservation_date.includes(' ') === true) {
+		return next({
+			status: 400,
+			message: `A reservation_date property must be a valid date.`,
+		});
+	}
+	if (reservation_date.includes('M') === true) {
+		return next({
+			status: 400,
+			message: `A reservation_date property must be a valid date.`,
+		});
+	}
+	if (reservation_date.includes('A') === true) {
+		return next({
+			status: 400,
+			message: `A reservation_date property must be a valid date.`,
+		});
+	}
+	if (reservation_date.includes('P') === true) {
+		return next({
+			status: 400,
+			message: `A reservation_date property must be a valid date.`,
+		});
+	}
+	return next();
+};
+
+/**
+ * Middleware to check for valid time
+ */
+const checkTimeValidity = (req, res, next) => {
+	const { reservation_time } = req.body.data || {};
+
+	if (reservation_time.length !== 5) {
+		return next({
+			status: 400,
+			message: `A reservation_time property must be a valid time.`,
+		});
+	}
+	if (reservation_time < '10:30') {
+		return next({
+			status: 400,
+			message: `A reservation_time property must be during business hours.`,
+		});
+	}
+	if (reservation_time > '21:30') {
+		return next({
+			status: 400,
+			message: `A reservation_time property must be during business hours.`,
+		});
+	}
 };
 
 /**
@@ -48,7 +203,7 @@ const create = async (req, res) => {
 			reservation_time,
 			people,
 		};
-		await service.create(newReservation);
+		await reservationsService.create(newReservation);
 		res.status(201).json({ data: newReservation });
 	} catch (error) {
 		res.status(500).json({ error: error.message });
@@ -57,5 +212,11 @@ const create = async (req, res) => {
 
 module.exports = {
 	list: [asyncErrorBoundary(reservationsExists), asyncErrorBoundary(list)],
-	create: asyncErrorBoundary(create),
+	create: [
+		checkRequiredFields,
+		checkTypes,
+		checkDateValidity,
+		checkTimeValidity,
+		asyncErrorBoundary(create),
+	],
 };
