9d19205311b3511282319a0edc426e8728f2f2bc
Ariela Israel <arielaisrael12@gmail.com>
2023-12-21 00:20:18 -0500

    seat btn added

--- front-end/src/reservations/ReservationsList.js
+++ front-end/src/reservations/ReservationsList.js
@@ -3,15 +3,20 @@ import React from 'react';
 const ReservationsList = ({ reservations, formatAsDate, formatAsTime }) => {
 	return (
 		<div>
-      {reservations.map((reservation) => (
-        <div key={reservation.reservation_id}>
-          <p>Name: {reservation.first_name} {reservation.last_name}</p>
-          <p>Phone Number: {reservation.mobile_number}</p>
-          <p>Reservation Date: {formatAsDate(reservation.reservation_date)}</p>
-          <p>Reservation Time: {formatAsTime(reservation.reservation_time)}</p>
-          <p>Party of {reservation.people}</p>
-        </div>
-      ))}
+			{reservations.map(reservation => (
+				<div key={reservation.reservation_id}>
+					<p>
+						Name: {reservation.first_name} {reservation.last_name}
+					</p>
+					<p>Phone Number: {reservation.mobile_number}</p>
+					<p>Reservation Date: {formatAsDate(reservation.reservation_date)}</p>
+					<p>Reservation Time: {formatAsTime(reservation.reservation_time)}</p>
+					<p>Party of: {reservation.people}</p>
+					<button type='button'>
+						<a href={`/reservations/${reservation.reservation_id}/seat`}>Seat</a>
+					</button>
+				</div>
+			))}
 		</div>
 	);
 };
