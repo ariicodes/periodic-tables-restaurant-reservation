46ec0d046967ead8459458db16916c2b9bca35c5
Ariela Israel <arielaisrael12@gmail.com>
2023-12-21 01:15:39 -0500

    component created

---
+++ front-end/src/tables/TablesList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const TablesList = ({ tables }) => {
+	return (
+		<div>
+			{tables.map(table => (
+				<div key={table.table_id}>
+					<p>Table Name: {table.table_name}</p>
+					<p>Capacity: {table.capacity}</p>
+					<p>Occupied: {table.reservation_id ? 'Occupied' : 'Free'}</p>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default TablesList;
