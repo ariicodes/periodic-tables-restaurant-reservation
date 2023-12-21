830d16d7a50e92c8fc2620edbb7a3a3d46fbadad
Ariela Israel <arielaisrael12@gmail.com>
2023-12-20 22:16:44 -0500

    code refactoring and middleware added

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -18,17 +18,33 @@ const reservationsExists = async (req, res, next) => {
 };
 
 /**
+ * Middleware to validate the existence of data
+ */
+const dataExists = async (req, res, next) => {
+	const { data } = req.body;
+	if (!data) {
+		return next({
+			status: 400,
+			message: `data is missing.`,
+		});
+	}
+	return next();
+};
+
+/**
  * Middleware to check for required fields
  */
 const checkRequiredFields = (req, res, next) => {
 	const {
-		first_name,
-		last_name,
-		mobile_number,
-		reservation_date,
-		reservation_time,
-		people,
-	} = req.body.data || {};
+		data: {
+			first_name,
+			last_name,
+			mobile_number,
+			reservation_date,
+			reservation_time,
+			people,
+		} = {},
+	} = req.body;
 
 	const requiredFields = {
 		first_name: 'A first_name property is required.',
@@ -55,13 +71,15 @@ const checkRequiredFields = (req, res, next) => {
  */
 const checkTypes = (req, res, next) => {
 	const {
-		first_name,
-		last_name,
-		mobile_number,
-		reservation_date,
-		reservation_time,
-		people,
-	} = req.body.data || {};
+		data: {
+			first_name,
+			last_name,
+			mobile_number,
+			reservation_date,
+			reservation_time,
+			people,
+		} = {},
+	} = req.body;
 
 	const typeChecks = {
 		first_name: 'string',
@@ -84,94 +102,138 @@ const checkTypes = (req, res, next) => {
 };
 
 /**
- * Middleware to check for valid date
+ * Middleware to check for valid date format
  */
-const checkDateValidity = (req, res, next) => {
-	const { reservation_date } = req.body.data || {};
-
-	if (reservation_date.length !== 10) {
-		return next({
-			status: 400,
-			message: `A reservation_date property must be a valid date.`,
-		});
-	}
-	if (reservation_date.includes('-') === false) {
-		return next({
-			status: 400,
-			message: `A reservation_date property must be a valid date.`,
-		});
-	}
-	if (reservation_date.includes('T') === true) {
-		return next({
-			status: 400,
-			message: `A reservation_date property must be a valid date.`,
-		});
-	}
-	if (reservation_date.includes(':') === true) {
-		return next({
-			status: 400,
-			message: `A reservation_date property must be a valid date.`,
-		});
-	}
-	if (reservation_date.includes('Z') === true) {
-		return next({
-			status: 400,
-			message: `A reservation_date property must be a valid date.`,
-		});
-	}
-	if (reservation_date.includes(' ') === true) {
-		return next({
-			status: 400,
-			message: `A reservation_date property must be a valid date.`,
-		});
-	}
-	if (reservation_date.includes('M') === true) {
-		return next({
-			status: 400,
-			message: `A reservation_date property must be a valid date.`,
-		});
-	}
-	if (reservation_date.includes('A') === true) {
-		return next({
-			status: 400,
-			message: `A reservation_date property must be a valid date.`,
-		});
-	}
-	if (reservation_date.includes('P') === true) {
-		return next({
-			status: 400,
-			message: `A reservation_date property must be a valid date.`,
-		});
+const checkDateFormat = (req, res, next) => {
+	const { data: { reservation_date } = {} } = req.body;
+	// Use regex to confirm that the date is formatted properly
+	const regex = /^\d{4}-\d{2}-\d{2}$/;
+	if (!regex.test(reservation_date)) {
+		throw { status: 400, message: 'reservation_date is invalid' };
 	}
 	return next();
+};
+
+/**
+ * Middleware to check if date falls on a Tuesday
+ */
+const checkTuesday = (req, res, next) => {
+	const { data: { reservation_date } = {} } = req.body;
+	// Split the date by '-' and convert the resulting substrings to numbers
+	const [year, month, day] = reservation_date.split('-').map(Number);
+	// Converts the date to the ISO 8601 format
+	const convertedDate = new Date(year, month - 1, day);
+
+	// Check if the date is a Tuesday; getDay() provides the day number where 2 = Tuesday via JavaScript months
+	if (convertedDate.getDay() === 2) {
+		throw {
+			status: 400,
+			message: 'Restaurant is closed on Tuesdays. Please choose another date.',
+		};
+	}
+	return next();
+};
+
+/**
+ * Middleware to check if date is in the past
+ */
+const checkPastDate = (req, res, next) => {
+	const { data: { reservation_date } = {} } = req.body;
+	// Split the date by '-' and convert the resulting substrings to numbers
+	const [year, month, day] = reservation_date.split('-').map(Number);
+	// Converts the date to the ISO 8601 format
+	const convertedDate = new Date(year, month - 1, day);
+
+	// Check if the date is in the past by comparing ISO formatted dates
+	const today = new Date();
+	today.setHours(0, 0, 0, 0);
+	if (convertedDate < today) {
+		throw { status: 400, message: 'Please choose a future date.' };
+	}
+	return next();
+};
+
+/**
+ * Middleware to time is outside of business hours (10:30am - 10:30pm)
+ */
+const checkBusinessHours = (inputTime, openingTime, closingTime) => {
+	if (inputTime < openingTime || inputTime >= closingTime) {
+		throw {
+			status: 400,
+			message: 'Reservation time is outside the restaurant operating hours.',
+		};
+	}
+};
+
+/**
+ * Middleware to check if time isn't right before closing
+ */
+const checkBeforeClosingTime = (inputTime, beforeClosingTime, closingTime) => {
+	if (inputTime > beforeClosingTime && inputTime < closingTime) {
+		throw {
+			status: 400,
+			message: 'Restaurant closes soon. Please choose an earlier time.',
+		};
+	}
+};
+
+/**
+ * Middleware to check if the time has passed
+ */
+const checkPastTime = (inputTimeStamp, currentTimeStamp) => {
+	if (inputTimeStamp <= currentTimeStamp) {
+		throw { status: 400, message: 'Please choose a future time.' };
+	}
 };
 
 /**
  * Middleware to check for valid time
  */
 const checkTimeValidity = (req, res, next) => {
-	const { reservation_time } = req.body.data || {};
+	const { data: { reservation_date, reservation_time } = {} } = req.body;
+	const regex = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
+	if (!regex.test(reservation_time)) {
+		throw { status: 400, message: 'reservation_time is invalid' };
+	}
 
-	if (reservation_time.length !== 5) {
-		return next({
-			status: 400,
-			message: `A reservation_time property must be a valid time.`,
-		});
-	}
-	if (reservation_time < '10:30') {
-		return next({
-			status: 400,
-			message: `A reservation_time property must be during business hours.`,
-		});
-	}
-	if (reservation_time > '21:30') {
-		return next({
-			status: 400,
-			message: `A reservation_time property must be during business hours.`,
-		});
-	}
+	const today = new Date();
+
+	// Converts the inputted time to an epoch timestamp
+	const dateObj = new Date(`${reservation_date} ${reservation_time}`);
+	const inputTime = Number(reservation_time.split(':').join(''));
+
+	const openingTime = 1030;
+	const beforeClosingTime = 2130;
+	const closingTime = 2230;
+
+	// Converts the inputted time to an epoch timestamp
+	const inputTimeStamp = dateObj.getTime();
+	// Converts the current time to an epoch timestamp
+	const currentTimeStamp = today.getTime();
+
+	checkBusinessHours(inputTime, openingTime, closingTime);
+	checkBeforeClosingTime(inputTime, beforeClosingTime, closingTime);
+	checkPastTime(inputTimeStamp, currentTimeStamp);
+
+	return next();
 };
 
+/**
+ * Middleware to check if people is zero
+ */
+const checkPeople = (req, res, next) => {
+	const { data: { people } = {} } = req.body;
+	if (people === 0) {
+		throw {
+			status: 400,
+			message: 'Please enter a number of people greater than zero.',
+		};
+	}
+	return next();
+};
+//////////////////////////////////////////////
+/// ---MIDDLEWARE ABOVE, HANDLERS BELOW--- ///
+//////////////////////////////////////////////
 /**
  * List handler for reservation resources
  */
@@ -213,10 +275,14 @@ const create = async (req, res) => {
 module.exports = {
 	list: [asyncErrorBoundary(reservationsExists), asyncErrorBoundary(list)],
 	create: [
+		dataExists,
 		checkRequiredFields,
 		checkTypes,
-		checkDateValidity,
+		checkDateFormat,
+		checkTuesday,
+		checkPastDate,
 		checkTimeValidity,
+		checkPeople,
 		asyncErrorBoundary(create),
 	],
 };
