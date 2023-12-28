22b552ba8c942a76e76a05d7579c3cb8809019f9
Ariela Israel <arielaisrael12@gmail.com>
2023-12-28 17:04:20 -0500

    TablesList created

--- front-end/src/tables/TablesList.js
+++ front-end/src/tables/TablesList.js
@@ -1,15 +1,26 @@
-import React from 'react';
-
-const TablesList = ({ tables }) => {
+const TablesList = ({ tables, handleFinish }) => {
 	return (
 		<div>
-			{tables.map(table => (
-				<div key={table.table_id}>
-					<p>Table Name: {table.table_name}</p>
-					<p>Capacity: {table.capacity}</p>
-					<p>Status: {table.reservation_id ? 'Occupied' : 'Free'}</p>
-				</div>
-			))}
+			{tables.map(table => {
+				return (
+					<div key={table.table_id}>
+						<p>Table Name: {table.table_name}</p>
+						<p>Capacity: {table.capacity}</p>
+						<p data-table-id-status={table.table_id}>
+							Status: {table.reservation_id ? 'Occupied' : 'Free'}
+						</p>
+						{table.reservation_id && (
+							<button
+								type='button'
+								data-table-id-finish={table.table_id}
+								onClick={() => handleFinish(table)}
+							>
+								Finish
+							</button>
+						)}
+					</div>
+				);
+			})}
 		</div>
 	);
 };
