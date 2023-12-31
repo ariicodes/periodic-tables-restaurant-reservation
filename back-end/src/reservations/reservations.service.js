957295880579fcb53c2760307c47baa6cc5a2b86
Ariela Israel <arielaisrael12@gmail.com>
2023-12-31 16:49:07 -0500

    update query add; list query update

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -8,6 +8,7 @@ const list = date => {
 	return knex('reservations')
 		.select('*')
 		.where({ reservation_date: dateQuery })
+		.whereNot({ status: 'finished' })
 		.orderBy('reservation_time');
 };
 
@@ -29,10 +30,21 @@ const read = async reservation_id => {
 		.select('*')
 		.where({ reservation_id })
 		.first();
-}
+};
+
+/**
+ * Update a reservation status
+ */
+const update = async (reservation_id, status) => {
+	return await knex('reservations')
+		.where({ reservation_id })
+		.update({ status }, '*')
+		.then(updatedRecords => updatedRecords[0]);
+};
 
 module.exports = {
 	list,
 	create,
 	read,
+	update,
 };
